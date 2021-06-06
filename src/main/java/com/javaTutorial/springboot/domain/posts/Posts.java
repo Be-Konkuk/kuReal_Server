97ecc37babe60bb493c5e230e593b6c8b8453cfc
Cyan00ffff <jungseoyun4@gmail.com>
2021-06-06 16:56:56 +0900

    api Testing

--- src/main/java/com/javaTutorial/springboot/domain/posts/Posts.java
+++ src/main/java/com/javaTutorial/springboot/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.javaTutorial.springboot.domain.posts;
 
+import com.javaTutorial.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,7 +15,7 @@ import javax.persistence.Id;
 @NoArgsConstructor
 @Entity
 
-public class Posts {
+public class Posts extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
@@ -33,5 +34,8 @@ public class Posts {
         this.content=content;
         this.author=author;
     }
-
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
 }
--- src/test/java/com/javaTutorial/springboot/ApplicationTest.java
+++ src/test/java/com/javaTutorial/springboot/ApplicationTest.java
@@ -1,4 +1,8 @@
 package com.javaTutorial.springboot;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+
 public class ApplicationTest {
 }
