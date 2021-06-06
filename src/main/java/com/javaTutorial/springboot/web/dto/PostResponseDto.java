0ff88e1ac39910b8a410a1c08be8c029dac95504
Cyan00ffff <50438923+Cyan00ffff@users.noreply.github.com>
2021-06-07 04:56:15 +0900

    fix errors

--- src/main/java/com/javaTutorial/springboot/web/dto/PostResponseDto.java
+++ src/main/java/com/javaTutorial/springboot/web/dto/PostResponseDto.java
@@ -5,12 +5,14 @@ import lombok.Getter;
 
 @Getter
 public class PostResponseDto {
+    private Long id;
     private String user;
     private String img;
     private String location;
     private String description;
 
     public PostResponseDto(Post entity){
+        this.id=entity.getId();
         this.user=entity.getUser();
         this.img=entity.getImg();
         this.location=entity.getLocation();
